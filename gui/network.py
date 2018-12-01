9dc1ca999cd46e3b621bfcf9978a1abede9a9e03
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-12-01 16:09:35 +0200

    made basic network work

--- gui/network.py
+++ gui/network.py
@@ -1,4 +1,30 @@
 import socket
+import threading
+import message
+
+class ListenThread(threading.Thread):
+
+    def __init__(self, reader, ui):
+        super().__init__()
+        self.reader = reader
+        self.ui = ui
+
+    def run(self):
+
+        try:
+            while True:
+                msg = message.to_message(self.reader.read_line())
+
+                if msg.type == "COMMAND" and msg.args[0] == "exit":
+                    self.ui.quit()
+                else:
+                    self.ui.append_message(msg.content)
+
+        finally:
+            print("connection was closed")
+
+
+
 
 
 def connect(address) -> socket.socket:
