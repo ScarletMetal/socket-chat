4e403cdfaa38cbca0c41a059631a0ac099123bf2
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-12-01 16:09:51 +0200

    made main in gui work

--- gui/app.py
+++ gui/app.py
@@ -1,18 +1,23 @@
 import tkinter
-from gui.gui import GUI
+from gui.ui import GUI
 import gui.network as network
+from reader import BufferReader
 
 def main():
 
     ip = input("Enter IP :: ")
-    port = input("Enter Port :: ")
+    port = int(input("Enter Port :: "))
 
     sock = network.connect((ip, port))
 
-    target = lambda s: sock.send(s.encode())
+    target = lambda s: sock.send("{}\n".format(s).encode())
 
-    gui = GUI(target=target)
-    gui.start()
+
+    ui = GUI(target=target)
+    listen_thread = network.ListenThread(BufferReader(sock.recv), ui)
+    listen_thread.start()
+    ui.request_prompt()
+    ui.start()
 
 
 if __name__ == '__main__':
